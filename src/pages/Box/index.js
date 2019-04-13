cf63b2ff9a60f937048b0e399f3a51d7ed9c2bf2
Ramoel Assis <ramoel@agileweb.com.br>
2019-04-13 19:39:21 -0400

    Ajustado link com HTTPS

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -23,7 +23,7 @@ export default class Box extends Component {
 
   subscribeToNewFiles = () => {
     const box = this.props.match.params.id;
-    const io = socket('http://easypdv.herokuapp.com/');
+    const io = socket('https://easypdv.herokuapp.com/');
 
     io.emit('connectRoom', box);
 
